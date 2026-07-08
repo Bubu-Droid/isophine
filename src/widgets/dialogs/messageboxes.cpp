3b681618c8f19b9f7c851119a186dfdd79734d11
Bubu-Droid <fufudadw@gmail.com>
2026-07-09 03:43:02 +0530

    feat: add message boxes

---
+++ src/widgets/dialogs/messageboxes.cpp
@@ -0,0 +1,56 @@
+#include "messageboxes.h"
+
+DBCorruptMsgBox::DBCorruptMsgBox(
+    QWidget* parent,
+    const QString& projListFilePath
+) :
+    QMessageBox(parent) {
+  this->setDefaultButton(QMessageBox::NoButton);
+  this->setWindowTitle(tr("Database Corrupted"));
+  this->setText(tr("The projects list database structure is malformed!"));
+  this->setInformativeText(
+      tr("Manual intervention needed, cannot run application.")
+  );
+  this->setDetailedText(
+      tr("Please open an issue on GitHub with the content of the projects list database file.\n\n"
+         "The file is present at the following path:\n"
+         "%1")
+          .arg(projListFilePath)
+  );
+  this->setIcon(QMessageBox::Critical);
+}
+
+FileReadFailedMsgBox::FileReadFailedMsgBox(
+    QWidget* parent,
+    const QString& projListFilePath
+) :
+    QMessageBox(parent) {
+  this->setDefaultButton(QMessageBox::NoButton);
+  this->setWindowTitle(tr("Failed To Read Database"));
+  this->setText(tr(
+      "Permission to read database file not granted, cannot run application."
+  ));
+  this->setDetailedText(
+      tr("Please add read permission flag to the projects list database file.\n\n"
+         "The file is present at the following path:\n"
+         "%1")
+          .arg(projListFilePath)
+  );
+  this->setIcon(QMessageBox::Critical);
+}
+
+SameProjectNameMsgBox::SameProjectNameMsgBox(QWidget* parent) :
+    QMessageBox(parent) {
+  this->setDefaultButton(QMessageBox::NoButton);
+  this->setWindowTitle(tr("Project Name Identical"));
+  this->setText(tr("Project names must be distinct!"));
+  this->setIcon(QMessageBox::Warning);
+}
+
+ProjSettingsIncompleteMsgBox::ProjSettingsIncompleteMsgBox(QWidget* parent) :
+    QMessageBox(parent) {
+  this->setDefaultButton(QMessageBox::NoButton);
+  this->setWindowTitle(tr("Incomplete Project Details"));
+  this->setText(tr("All entries are compulsory, they must not be empty!"));
+  this->setIcon(QMessageBox::Warning);
+}
---
+++ src/widgets/dialogs/messageboxes.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <QMessageBox>
+
+class DBCorruptMsgBox: public QMessageBox {
+  Q_OBJECT
+
+public:
+  DBCorruptMsgBox() = delete;
+  explicit DBCorruptMsgBox(
+      QWidget* parent = nullptr,
+      const QString& projListFilePath = ""
+  );
+};
+
+class FileReadFailedMsgBox: public QMessageBox {
+  Q_OBJECT
+
+public:
+  FileReadFailedMsgBox() = delete;
+  explicit FileReadFailedMsgBox(
+      QWidget* parent = nullptr,
+      const QString& projListFilePath = ""
+  );
+};
+
+class SameProjectNameMsgBox: public QMessageBox {
+  Q_OBJECT
+
+public:
+  explicit SameProjectNameMsgBox(QWidget* parent = nullptr);
+};
+
+class ProjSettingsIncompleteMsgBox: public QMessageBox {
+  Q_OBJECT
+
+public:
+  explicit ProjSettingsIncompleteMsgBox(QWidget* parent = nullptr);
+};
